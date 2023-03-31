b6e41e3577763420927260f9c8c5e4da0edf6a6b
kingrex0830 <ycea0830@yahoo.com>
2023-03-31 13:06:29 -0400

    Added timer and leaderboard with full functionality

--- Assets/script.js
+++ Assets/script.js
@@ -4,6 +4,7 @@ startButton.addEventListener("click", startQuiz)
 var nextButton1 = document.getElementById("next-1")
 var nextButton2 = document.getElementById("next-2")
 var finishButton = document.getElementById("finish")
+var restartButton = document.getElementById("restart")
 nextButton1.addEventListener("click", nextQuestion1)
 nextButton2.addEventListener("click", nextQuestion2)
 finishButton.addEventListener("click", finishQuiz)
@@ -25,7 +26,11 @@ var scoreCount = 0
 var userScore = document.getElementById("user-score")
 var profileSetup = document.getElementById("profile-setup")
 var initialsInput = document.getElementById("user-initials")
-var submitButton = document.getElementById("submit")
+var submitButton = document.getElementById("submit-button")
+var count = 60
+var timeLeft = document.getElementById("time-left")
+var leaderboardList = document.getElementById("leaderboard-list")
+console.log(initialsInput.value)
 
 function addScore1() {
     scoreCount = scoreCount + 1
@@ -69,6 +74,7 @@ function noScore1() {
   incorrectAnswer12.disabled = true
   incorrectAnswer11.textContent = "Incorrect!"
   incorrectAnswer12.textContent = "Incorrect!"
+  count -= 5
 }
 
 function noScore2() {
@@ -77,6 +83,7 @@ function noScore2() {
   incorrectAnswer22.disabled = true
   incorrectAnswer21.textContent = "Incorrect!"
   incorrectAnswer22.textContent = "Incorrect!"
+  count -= 5
 }
 
 function noScore3() {
@@ -85,6 +92,7 @@ function noScore3() {
   incorrectAnswer32.disabled = true
   incorrectAnswer31.textContent = "Incorrect!"
   incorrectAnswer32.textContent = "Incorrect!"
+  count -= 5
 }
 
 function isIncorrect() {
@@ -96,13 +104,35 @@ function isIncorrect() {
   incorrectAnswer32.addEventListener("click", noScore3)
 }
 
+function timer() {
+  const makeIteration = () => {
+    // console.clear();
+    if (count > 0) {
+      console.log(count);
+      setTimeout(makeIteration, 1000); // 1 second waiting
+    } else if (count == 0) {
+      finishQuiz()
+      return
+    }
+    count -= 1;
+    timeLeft.textContent=count
+  }
+  
+  setTimeout(makeIteration, 1000); // 1 second waiting
+  console.log(makeIteration)
+  console.log(count)
+  }
+
 function startQuiz() {
   console.log("Start")
+  count=60
+  timer()
   isCorrect()
   isIncorrect()
   startMenu.style.display="none"
   nextButton1.style.display="block"
   question1.style.display="block"
+  timeLeft.style.display="block"
   console.log("Hello world")
 }
 
@@ -133,19 +163,37 @@ function finishQuiz() {
   userScore.style.display="flex"
   profileSetup.style.display="block"
   submitButton.style.display="block"
+  restartButton.style.display="block"
+  timeLeft.style.display="none"
   isCorrect()
   isIncorrect()
   userScore.textContent += scoreCount
   console.log(scoreCount)
 }
 
-function countdownTimer() {
-  var count = 60, timer = setInterval(function() {
-    $("#counter").html(count--)
-    if(count == 1) clearInterval(timer)
-}, 1000);
-
-// if(wrong answer) {
-//  -5sec
-// }
+function saveProfile(event) {
+  event.preventDefault()
+  console.log("hello")
+  var userInitials = initialsInput.value
+  console.log(userInitials)
+  console.log("score is ", scoreCount)
+  var score = {
+    finalScore: scoreCount,
+    user: userInitials
+  }
+  console.log(score)
+  localStorage.setItem("score", JSON.stringify(score))
+  score = JSON.parse(localStorage.getItem('score'))
+  leaderboardList.textContent = "Initials:" + score.user + " Score: " + score.finalScore
 }
+
+function scoreboard() {
+  
+}
+
+function restartQuiz() {
+  // location.reload()
+}
+
+// restartButton.addEventListener("click", restartQuiz)
+submitButton.addEventListener("click", saveProfile)
